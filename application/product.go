d56bac1456c76b9cbd35cf6f822004fc3f2dc29f
Danilo Bandeira <danilo.bandeira@growthtech.com.br>
2022-09-06 07:37:37 -0300

    feat: able to disable the product when price is equal to zero

--- application/product.go
+++ application/product.go
@@ -37,6 +37,10 @@ func (p *Product) Enable() error {
 }
 
 func (p *Product) Disable() error {
+	if p.Price == 0 {
+		p.Status = DISABLED
+		return nil
+	}
 	return errors.New("the price must be zero in order to have the product disabled")
 }
 
