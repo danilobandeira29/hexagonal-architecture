3239dcbd624992f4e33098003c9fedf43449df93
Danilo Bandeira <danilo.bandeira@growthtech.com.br>
2022-09-07 09:54:06 -0300

    feat: add create func

--- application/product_service.go
+++ application/product_service.go
@@ -11,3 +11,18 @@ func (s *ProductService) Get(id string) (ProductInterface, error) {
 	}
 	return p, nil
 }
+
+func (s *ProductService) Create(name string, price float64) (ProductInterface, error) {
+	p := NewProduct()
+	p.Name = name
+	p.Price = price
+	_, err := p.IsValid()
+	if err != nil {
+		return &Product{}, err
+	}
+	product, err := s.Persistence.Save(p)
+	if err != nil {
+		return &Product{}, err
+	}
+	return product, nil
+}
