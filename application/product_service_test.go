bb1827f178d4067ce8719215a9732b8aef02fd4a
Danilo Bandeira <danilo.bandeira@growthtech.com.br>
2022-09-07 09:24:24 -0300

    test: err when persistence return err

--- application/product_service_test.go
+++ application/product_service_test.go
@@ -1,6 +1,7 @@
 package application_test
 
 import (
+	"errors"
 	"github.com/danilobandeira29/hexagonal-architecture/application"
 	mock_application "github.com/danilobandeira29/hexagonal-architecture/application/mocks"
 	"github.com/golang/mock/gomock"
@@ -20,4 +21,17 @@ func TestProductService_Get_Success(t *testing.T) {
 	result, err := service.Get("1234")
 	require.Nil(t, err)
 	require.Equal(t, product, result)
+}
+
+func TestProductService_Get_ErrWhenPersistenceReturnErr(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	persistence := mock_application.NewMockProductPersistenceInterface(ctrl)
+	persistence.EXPECT().Get(gomock.Any()).Return(nil, errors.New("error from persistence")).AnyTimes()
+	service := application.ProductService{
+		Persistence: persistence,
+	}
+	result, err := service.Get("1234")
+	require.Nil(t, result)
+	require.Error(t, err)
 }
