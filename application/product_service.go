cbe0b62c7bc1f16c82f3dedebbd89befddeaca69
Danilo Bandeira <danilo.bandeira@growthtech.com.br>
2022-09-07 10:03:17 -0300

    feat: add enable func

--- application/product_service.go
+++ application/product_service.go
@@ -26,3 +26,15 @@ func (s *ProductService) Create(name string, price float64) (ProductInterface, e
 	}
 	return product, nil
 }
+
+func (s *ProductService) Enable(p ProductInterface) (ProductInterface, error) {
+	err := p.Enable()
+	if err != nil {
+		return &Product{}, err
+	}
+	product, err := s.Persistence.Save(p)
+	if err != nil {
+		return &Product{}, err
+	}
+	return product, nil
+}
