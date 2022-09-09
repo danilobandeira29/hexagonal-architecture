27046c9d49bbe81e9810613a2e8796d05ad23fc0
Danilo Bandeira <danilobandeira29@gmail.com>
2022-09-09 11:42:29 -0300

    test: test product db get

--- adapter/db/product_test.go
+++ adapter/db/product_test.go
@@ -2,7 +2,10 @@ package db_test
 
 import (
 	"database/sql"
+	"github.com/danilobandeira29/hexagonal-architecture/adapter/db"
+	"github.com/stretchr/testify/require"
 	"log"
+	"testing"
 )
 
 var Db *sql.DB
@@ -18,7 +21,7 @@ func createTable(db *sql.DB) {
 				"id" string,
 				"name" string,
 				"price" float,
-				"status" string,
+				"status" string
 				);
 `)
 	if err != nil {
@@ -33,4 +36,16 @@ func createProduct(db *sql.DB) {
 		log.Fatal(err.Error())
 	}
 	stmt.Exec()
+}
+
+func TestProductDb_Get(t *testing.T) {
+	setupDb()
+	productDb := db.NewProductDb(Db)
+	defer Db.Close()
+	product, err := productDb.Get("1234")
+	require.Nil(t, err)
+	require.Equal(t, "1234", product.GetID())
+	require.Equal(t, "Product 1", product.GetName())
+	require.Equal(t, 40.0, product.GetPrice())
+	require.Equal(t, "disabled", product.GetStatus())
 }
