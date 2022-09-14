3639ca2cf551c16334c551518c32bd508fd78aca
Danilo Bandeira <danilobandeira29@gmail.com>
2022-09-14 12:50:15 -0300

    feat: add web server

---
+++ adapter/web/server/server.go
@@ -0,0 +1,37 @@
+package server
+
+import (
+	"github.com/codegangsta/negroni"
+	"github.com/danilobandeira29/hexagonal-architecture/application"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+type Webserver struct {
+	service application.ProductServiceInterface
+}
+
+func NewWebserver(service application.ProductServiceInterface) *Webserver {
+	return &Webserver{
+		service,
+	}
+}
+
+func (w *Webserver) Serve() {
+	r := mux.NewRouter()
+	n := negroni.New(negroni.NewLogger())
+	server := &http.Server{
+		ReadHeaderTimeout: 10 * time.Second,
+		WriteTimeout:      10 * time.Second,
+		Addr:              ":8080",
+		Handler:           http.DefaultServeMux,
+		ErrorLog:          log.New(os.Stderr, "log:", log.Lshortfile),
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
