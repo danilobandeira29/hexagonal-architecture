16f63e307a8d5d01c250d5dc6bcf871e8696f905
Danilo Bandeira <danilobandeira29@gmail.com>
2022-09-16 11:28:54 -0300

    feat: change port

--- adapter/web/server/server.go
+++ adapter/web/server/server.go
@@ -12,24 +12,22 @@ import (
 )
 
 type Webserver struct {
-	service application.ProductServiceInterface
+	Service application.ProductServiceInterface
 }
 
-func NewWebserver(service application.ProductServiceInterface) *Webserver {
-	return &Webserver{
-		service,
-	}
+func NewWebserver() *Webserver {
+	return &Webserver{}
 }
 
 func (w *Webserver) Serve() {
 	r := mux.NewRouter()
 	n := negroni.New(negroni.NewLogger())
-	handler.MakeProductHandlers(r, n, w.service)
+	handler.MakeProductHandlers(r, n, w.Service)
 	http.Handle("/", r)
 	server := &http.Server{
 		ReadHeaderTimeout: 10 * time.Second,
 		WriteTimeout:      10 * time.Second,
-		Addr:              ":8080",
+		Addr:              ":9000",
 		Handler:           http.DefaultServeMux,
 		ErrorLog:          log.New(os.Stderr, "log:", log.Lshortfile),
 	}
